498fb3311ddc7539c5ce2a5f105ad7dbc60e0548
angre-kim <angre@daum.net>
2020-01-03 11:11:23 +0900

    커밋메세지

---
+++ android/app/src/main/kotlin/com/angre/xylo_git_0103/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.angre.xylo_git_0103
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
